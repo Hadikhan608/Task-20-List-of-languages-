a2f7665c15b18b20bda0a35d4a973c2cfb2962af
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-06-05 12:41:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Task 20: Think of something like mountains, cars, counties and store in an 
+//array then print a program that contains list of this array.
+let language = ["Urdu", "English", "Chinese", "Italian", "Persian"];
+console.log("list of languages:");
+console.log(language);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Task 20: Think of something like mountains, cars, counties and store in an 
+//array then print a program that contains list of this array.
+
+let language:string []= ["Urdu","English","Chinese","Italian","Persian"];
+console.log("list of languages:");
+console.log(language);
